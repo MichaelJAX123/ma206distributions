7796318417fbc2cac201c24637681b8facad9f6b
EvaMaeRey <evangeline.mae@gmail.com>
2023-03-01 12:26:02 -0500

    scale counting

--- R/scale_x_counting.R
+++ R/scale_x_counting.R
@@ -7,10 +7,18 @@
 #' @export
 #'
 #' @examples
+#' library(ggplot2)
+#' ggplot(tidy_dbinom(1/6, num_trials = 8)) +
+#'   aes(x = num_successes) +
+#'   scale_x_counting() +
+#'   aes(y = probability) +
+#'   geom_lollipop()
+#'
 scale_x_counting <- function(breaks = 0:10000, ...){
 
   ggplot2::scale_x_continuous(breaks = breaks, ...)
 
 }
+
 
 
